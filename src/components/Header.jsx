b38c1feaafc7dcd57673d97bcb9b10aa3499855f
TharinduNilan <tharindunilan04@gmail.com>
2024-03-24 16:14:56 +0530

    Create the Light Mode and Dark Mode. 	# modified:   package-lock.json 	# modified:   package.json 	# new file:   src/app/Providers.jsx 	# modified:   src/app/layout.js 	# new file:   src/components/DarkModeSwitch.jsx 	# modified:   src/components/Header.jsx 	# modified:   tailwind.config.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import MenuItem from './MenuItem';
 import {AiFillHome} from 'react-icons/ai';
 import {BsFillInfoCircleFill} from 'react-icons/bs';
+import DarkModeSwitch from './DarkModeSwitch';
 
 export default function Header() {
   return (
@@ -10,10 +11,13 @@ export default function Header() {
             <MenuItem title='Home' address='/' Icon={AiFillHome}/>
             <MenuItem title='About' address='/about' Icon={BsFillInfoCircleFill}/>
         </div>
-        <Link href={'/'} className="flex gap-1 items-center">
-            <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>Movie</span>
-            <span className='text-xl hidden sm:inline'>NTN</span>
-        </Link>
+        <div className="flex gap-4 items-center">
+            <DarkModeSwitch />
+            <Link href={'/'} className="flex gap-1 items-center">
+                    <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>Movie</span>
+                    <span className='text-xl hidden sm:inline'>NTN</span>
+            </Link>
+        </div>
     </div>
   )
 }
