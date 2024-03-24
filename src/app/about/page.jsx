82cf644bbf6425c266857d065a5dd193f59efd93
TharinduNilan <tharindunilan04@gmail.com>
2024-03-24 16:43:42 +0530

    Created the about page.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate assumenda libero ab repellat asperiores error corporis nihil commodi dolorum placeat quia voluptatibus ut, distinctio laudantium accusamus facilis optio quod expedita! , Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda commodi vero voluptates, ut minima mollitia laboriosam facilis suscipit eveniet cum dolorem aspernatur sunt facere, excepturi labore repellat. Ipsum, aliquam distinctio?</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam velit delectus eos numquam illo repellendus nisi, voluptatum magnam ex quidem nam exercitationem deleniti quia molestiae placeat quis rem. Vero, dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, accusantium vero dolore eligendi porro libero. Quod ipsam quia deleniti odio nam. Dolore, nostrum officia doloribus eaque est tempora eos animi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium doloremque commodi numquam rerum maiores nobis magnam unde quaerat magni! Itaque accusantium quidem ex molestias praesentium asperiores fugit ad laboriosam beatae. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates facere impedit dolor veritatis tempore et, delectus sapiente libero sint aliquam asperiores quasi rerum atque dolorum corrupti modi alias, quis consequatur?</p>
+    </div>
+  )
+}
